cd0dc9434ac45e86c6249fbfd162984e2edbea0a
WilliamSSR <3088939133@qq.com>
2026-03-15 01:17:01 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CycleWise;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CycleWise.Pages;
+
+namespace CycleWise;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+        Routing.RegisterRoute(nameof(LogPage), typeof(LogPage));
+        Routing.RegisterRoute(nameof(HistoryPage), typeof(HistoryPage));
+    }
+}
